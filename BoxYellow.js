69d012ba677efcbc5928a952c24ecd6cd33feaec
TanmayKhurana <74850207+TanmayKhurana@users.noreply.github.com>
2021-08-27 22:42:54 +0530

    Add files via upload

---
+++ BoxYellow.js
@@ -0,0 +1,30 @@
+class BoxYellow{
+    constructor(x,y,w,h){
+        var r_options={
+            isStatic:0
+        }
+
+        this.r = Bodies.rectangle(x,y,w,h, r_options);
+        this.w = w
+        this.h = h
+        this.visibility =255
+ 	    World.add(world, this.r);
+
+    }
+    display(){
+        console.log(this.r.speed)
+        if(this.r.speed<10){
+            var pos = this.r.position
+            push()
+            translate(pos.x, pos.y)
+            rectMode(CENTER)
+        fill("yellow");rect(0, 0, this.w, this.h)
+        pop()
+        }else{
+            World.remove(world, this.r)
+            this.visibility = this.visibility-10
+        }
+        
+        
+    }
+}
